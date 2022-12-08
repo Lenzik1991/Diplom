e730026b5676b5bd3df77487c01d5ee6e843e2d0
Елена <lenzik_1991@mail.ru>
2022-12-08 23:07:09 +0500

    modified: ../docker-compose-ci.yml ../docker-compose.yml ../requirements.txt core/views.py todolist/settings.py todolist/urls.py

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'environ',
     'rest_framework',
     'corsheaders',
+    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -139,6 +140,19 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.vk.VKOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
 AUTH_USER_MODEL = 'core.User'
 
-CORS_ALLOW_ALL_ORIGINS = True
+SOCIAL_AUTH_JSONFIELD_ENABLED = True
+SOCIAL_AUTH_VK_SCOPE = ['email']
+SOCIAL_AUTH_VK_OAUTH2_KEY = env.str('SOCIAL_AUTH_VK_OAUTH2_KEY')
+SOCIAL_AUTH_VK_OAUTH2_SECRET = env.str('SOCIAL_AUTH_VK_OAUTH2_SECRET')
+SOCIAL_AUTH_URL_NAMESPACE = 'social'
+SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/logged-in/' #52 02
+SOCIAL_AUTH_LOGIN_ERROR_URL = '/login-error/'
+
+# CORS_ALLOW_ALL_ORIGINS = True
