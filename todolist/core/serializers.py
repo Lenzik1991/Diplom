b9a024584debb20c2d2b8f61cefee8ed62f1182c
Елена <lenzik_1991@mail.ru>
2022-12-20 22:06:55 +0500

    modified: core/serializers.py

--- todolist/core/serializers.py
+++ todolist/core/serializers.py
@@ -11,35 +11,69 @@ from rest_framework.exceptions import ValidationError, AuthenticationFailed, Not
 USER_MODEL = get_user_model()
 
 
+class PasswordField(serializers.CharField):
+
+    def __init__(self, **kwargs):
+        kwargs['style'] = {'input_type': 'password'}
+        kwargs.setdefault('write_only', True)
+        super().__init__(**kwargs)
+        self.validators.append(validate_password)
+
+
 class RegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    password_repeat = serializers.CharField(write_only=True)
-
-    def validate(self, attrs):
-        # if attrs['password'] != attrs['password_repeat']:
-        #     raise ValidationError("password and password_repeat is not equal")
-        # return attrs
-        password = attrs.get('attrs')
-        password_repeat = attrs.pop('password_repeat')
-
-        try:
-            validate_password(password)
-        except Exception as e:
-            raise serializers.ValidationError({'password': e.args[0]})
-
-        if password != password_repeat:
-            raise serializers.ValidationError("Passwords don't match")
-        return attrs
-
-    def create(self, validated_data):
-        password = validated_data.get('password')
-        validated_data['password'] = make_password(password)
-        instance = super().create(validated_data)
-        return instance
+    password = PasswordField(required=True)
+    password_repeat = PasswordField(required=True)
 
     class Meta:
         model = USER_MODEL
-        fields = '__all__'
+        read_only_fields = ("id",)
+        fields = (
+            "id",
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "password",
+            "password_repeat",
+        )
+
+    def validate(self, attrs: dict):
+        if attrs['password'] != attrs['password_repeat']:
+            raise ValidationError("password and password_repeat is not equal")
+        return attrs
+
+    def create(self, validated_data: dict):
+        del validated_data['password_repeat']
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
+    # password = serializers.CharField(write_only=True)
+    # password_repeat = serializers.CharField(write_only=True)
+    #
+    # def validate(self, attrs):
+    #     # if attrs['password'] != attrs['password_repeat']:
+    #     #     raise ValidationError('password and password_repeat is not equal')
+    #     # return attrs
+    #     password = attrs.get('attrs')
+    #     password_repeat = attrs.pop('password_repeat')
+    #
+    #     try:
+    #         validate_password(password)
+    #     except Exception as e:
+    #         raise serializers.ValidationError({'password': e.messages})
+    #
+    #     if password != password_repeat:
+    #         raise serializers.ValidationError('Passwords do not match')
+    #     return attrs
+    #
+    # def create(self, validated_data):
+    #     password = validated_data.get('password')
+    #     validated_data['password'] = make_password(password)
+    #     instance = super().create(validated_data)
+    #     return instance
+    #
+    # class Meta:
+    #     model = USER_MODEL
+    #     fields = '__all__'
 
 
 class LoginSerializer(serializers.ModelSerializer):
