dce17cc51ff6028525eac9e0eec7202bac062062
Елена <lenzik_1991@mail.ru>
2022-12-28 17:09:46 +0500

    modified goals/serializers.py

--- todolist/goals/serializers.py
+++ todolist/goals/serializers.py
@@ -79,20 +79,20 @@ class GoalSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ('id', 'created', 'updated', 'user')
 
-    # def validate_category(self, value):
-    #     if value.is_deleted:
-    #         raise serializers.ValidationError("not allowed in deleted category")
-    #
-    #     if value.user != self.context["request"].user:
-    #         raise serializers.ValidationError("not owner of category")
-    #     return value
-
     def validate_category(self, value):
+        if value.is_deleted:
+            raise serializers.ValidationError('not allowed in deleted category')
 
         if value.user != self.context['request'].user:
             raise serializers.ValidationError('not owner of category')
-
         return value
+
+    # def validate_category(self, value):
+    #
+    #     if value.user != self.context['request'].user:
+    #         raise serializers.ValidationError('not owner of category')
+    #
+    #     return value
 
 
 class GoalCommentCreateSerializer(serializers.ModelSerializer):
