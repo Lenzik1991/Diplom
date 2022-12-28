6d08a4e4a7981b395f7f6a24ef78f916241577f1
Елена <lenzik_1991@mail.ru>
2022-12-28 21:24:23 +0500

    modified BaseModel goals/models.py

--- todolist/goals/models.py
+++ todolist/goals/models.py
@@ -5,17 +5,23 @@ from core.models import User
 
 
 class BaseModel(models.Model):
+    created = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    updated = models.DateTimeField(verbose_name='Дата последнего обновления', auto_now=True)
+
     class Meta:
         abstract = True
-
-    created = models.DateTimeField(verbose_name="Дата создания")
-    updated = models.DateTimeField(verbose_name="Дата последнего обновления")
-
-    def save(self, *args, **kwargs):
-        if not self.id:
-            self.created = timezone.now()
-        self.updated = timezone.now()
-        return super().save(*args, **kwargs)
+# class BaseModel(models.Model):
+#     class Meta:
+#         abstract = True
+#
+#     created = models.DateTimeField(verbose_name="Дата создания")
+#     updated = models.DateTimeField(verbose_name="Дата последнего обновления")
+#
+#     def save(self, *args, **kwargs):
+#         if not self.id:
+#             self.created = timezone.now()
+#         self.updated = timezone.now()
+#         return super().save(*args, **kwargs)
 
 
 class Board(BaseModel):
